adb77bc6335e2846d9219ab96978395478fd43dc
Li Yun <leven.cn@gmail.com>
2015-08-21 14:09:06 +0800

    增加TCP KeepAlive配置脚本

--- admin/aliyun-admin.py
+++ admin/aliyun-admin.py
@@ -21,6 +21,10 @@
 
 - CentOS 7.0
 
+支持的操作：
+
+- 更新Python 3环境
+
 '''
 
 import sys
@@ -35,7 +39,7 @@ def shell_output(cmd):
     @param cmd shell command
     @return result string
     '''
-    return subprocess.check_output(cmd, shell=True).decode('utf-8')
+    return subprocess.check_output(cmd, shell=True, universal_lines=True)
 
 
 def cpu_cores():
@@ -51,6 +55,67 @@ def cpu_cores():
     #     cat /proc/cpuinfo| grep "processor" | uniq | wc -l
     logical_cores = int(shell_output('cat /proc/cpuinfo | grep "processor" | uniq | wc -l'))
     return physical_cores, logical_cores
+
+
+def get_default_tcp_keep_alive(t=None, intvl=None, probes=None):
+    '''配置Linux系统默认的TCP Keep-Alive。
+
+    @param t tcp_keepalive_time值，如果不为None，则修改成该值；如果为None，返回当前系统默认值
+    @param intvl keep_alive_intvl值，如果不为None，则修改成该值；如果为None，返回当前系统默认值
+    @param probes keep_alive_probes值，如果不为None，则修改成该值；如果为None，返回当前系统默认值
+    @return a tuple of tcp_keepalive_time, keep_alive_intvl and keep_alive_probes
+    @exception ValueError parameter error
+
+    <h2>TCP Keep-Alive机制</h2>
+    1. 每隔tcp_keepalive_time秒，发送心跳包。
+    2. 如果主机可达，对方就会响应ACK应答，就认为是存活的。
+    3. 如果可达，但应用程序退出，对方就发RST应答，发送TCP撤消连接。
+    4 .如果可达，但应用程序崩溃，对方就发FIN消息。
+    5. 如果对方主机不响应ACK, RST，每隔tcp_keepalive_intvl秒继续发送tcp_keepalive_probes次直到超时，就撤消连接。
+
+    # 查看探测间隔时间（秒）
+    $ sysctl net.ipv4.tcp_keepalive_time
+    net.ipv4.tcp_keepalive_time = 7200
+
+    # 查看当不可达时，重复发送探测的时间间隔（秒）
+    $ sysctl net.ipv4.tcp_keepalive_intvl
+    net.ipv4.tcp_keepalive_intvl = 75
+
+    # 查看当不可达时，重复发送探测的次数
+    $ sysctl net.ipv4.tcp_keepalive_probes
+    net.ipv4.tcp_keepalive_probes = 9
+    
+    # 永久修改 /etc/sysctl.conf
+    #
+    #     net.ipv4.tcp_keepalive_time=1800
+    #     net.ipv4.tcp_keepalive_intvl=1
+    #     net.ipv4.tcp_keepalive_probes=9
+    #
+    # 使其生效
+    $ sudo sysctl -p /etc/sysctl.conf
+    ## Ubuntu/Debian
+    ## $ sudo service procps restart
+    '''
+    # 修改
+    if t is not None:
+        if t < 0:
+            raise ValueError('net.ipv4.tcp_keepalive_time 不能设置成负数')
+        shell_output('sudo sysctl -w net.ipv4.tcp_keepalive_time={}'.format(t))
+    if intvl is not None:
+        if intvl < 0:
+            raise ValueError('net.ipv4.tcp_keepalive_intvl 不能设置成负数')
+        shell_output('sudo sysctl -w net.ipv4.tcp_keepalive_intvl={}'.format(intvl))
+    if probes is not None:
+        if probes < 0:
+            raise ValueError('net.ipv4.tcp_keepalive_probes 不能设置成负数')
+        shell_output('sudo sysctl -w net.ipv4.tcp_keepalive_probes={}'.format(probes))
+
+    # 获取默认值
+    keep_alive_time = int(shell_output('sysctl net.ipv4.tcp_keepalive_time'))
+    keep_alive_intvl = int(shell_output('sysctl net.ipv4.tcp_keepalive_intvl'))
+    keep_alive_probes = int(shell_output('sysctl net.ipv4.tcp_keepalive_probes'))
+
+    return keep_alive_time, keep_alive_intvl, keep_alive_probes
 
 
 def build_python_from_source(logical_cpu_cores=1):
@@ -76,9 +141,9 @@ if __name__ == '__main__':
         sys.exit('Only support Linux')
 
     # 设置命令行参数
-    parser = argparse.ArgumentParser(description='Rebuild Pythn 3 environment.')
+    parser = argparse.ArgumentParser(description='Administrate Aliyun ECS.')
     parser.add_argument('commands', metavar='command', type=str, nargs='+',
-                   help='a command to be executed, one of "user", "upgrade-python", "upgrade-pip"')
+                   help='a command to be executed, one of "upgrade-python", "upgrade-pip"')
     parser.add_argument('--foo', help='foo help')
 
     # 解析命令行参数
